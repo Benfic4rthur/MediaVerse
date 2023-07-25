9de3b0048f81708129c0b202d5a2494df02b03bd
joaolucas3002 <92446800+joaolucas3002ht@users.noreply.github.com>
2023-07-25 12:40:03 -0300

    fix: adição dos textos

--- src/components/Access/Functionality/index.jsx
+++ src/components/Access/Functionality/index.jsx
@@ -8,24 +8,40 @@ export function Functionality() {
     <>
       <H2 id='Functionality'>Funcionalidades</H2>
       <ContainerCardFunctionality>
-        <CardFunctionality title={'lorem lorem lorem '} description={'texto '} typeAnimated='left'>
+        <CardFunctionality
+          title={'Controle completo do usuário'}
+          description={
+            'Tenha total controle e acesso aos conteúdos que foram acessados pelos usuários. Faça novos cadastros, alterações, exclusões e banimentos dos usuários.'
+          }
+          typeAnimated='left'
+        >
           <Image src='' alt='' />
         </CardFunctionality>
         <CardFunctionality
           reverse
-          title={'lorem lorem lorem '}
-          description={'texto '}
+          title={'Controle e redirecionamento de conteúdo'}
+          description={
+            'Agende webnarios com antecência, avise os participantes e faça o envio do link e disponibilização do login e senha para utilização momentanea.'
+          }
           typeAnimated='right'
         >
           <Image src='' alt='' />
         </CardFunctionality>
-        <CardFunctionality title={'lorem lorem lorem '} description={'texto'} typeAnimated='left'>
+        {/* <CardFunctionality
+          title={'Webnário'}
+          description={
+            'Agende webnarios com antecência, avise os participantes e faça o envio do link e disponibilização do login e senha para utilização momentanea.'
+          }
+          typeAnimated='left'
+        >
           <Image src='' alt='' />
-        </CardFunctionality>
+        </CardFunctionality> */}
         <CardFunctionality
-          reverse
-          title={'lorem lorem lorem '}
-          description={'texto '}
+          // reverse
+          title={'Cursos sob demanda'}
+          description={
+            'Possibilidade de disponibilizar o conteúdo da sua faculdade, curso técnico, treinamento empresárial, reciclagem corporativa, etc. Com a possibilidade de testar o conhecimento adquirido pelo aluno.'
+          }
           typeAnimated='right'
         >
           <Image src='' alt='' />
--- src/components/Access/Header/index.jsx
+++ src/components/Access/Header/index.jsx
@@ -2,13 +2,19 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { Link } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
-import { Navbar, Header as HeaderStyled } from './styled';
+import { Header as HeaderStyled, Navbar } from './styled';
 
+import { useEffect, useState } from 'react';
+import { LuMenu, LuX } from 'react-icons/lu';
 import logo from '../../../assets/logo.png';
-import { useState, useEffect } from 'react';
+import { ContainerAdaptiveMenu, MobileMenuToggle, Nav } from '../../Navbar/styled';
 
 export function Header() {
   const [title, setTitle] = useState('- Início');
+  const [expanded, setExpanded] = useState(false);
+  const toggleMenu = () => {
+    setExpanded(!expanded);
+  };
 
   useEffect(() => {
     document.title = `MediaVerse ${title}`;
@@ -31,63 +37,69 @@ export function Header() {
             <img src={logo} alt='Logo' title='MediaVerse' onClick={() => setTitle('- Início')} />
           </LinkScroll>
         </div>
-        <div>
-          <LinkScroll
-            to='Functionality'
-            spy={true}
-            smooth={true}
-            offset={-80}
-            duration={750}
-            title='Início'
-            onClick={() => setTitle('- Início')}
-            className='link'
-            style={{ cursor: 'pointer' }}
-          >
-            <h2>Início</h2>
-          </LinkScroll>
-          <LinkScroll
-            to='Functionality'
-            spy={true}
-            smooth={true}
-            offset={-80}
-            duration={600}
-            title='Funcionalidades'
-            onClick={() => setTitle('- Funcionalidades')}
-            className='link'
-            style={{ cursor: 'pointer' }}
-          >
-            <h2>Funcionalidades</h2>
-          </LinkScroll>
-          <LinkScroll
-            to='TargetSectors'
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={1000}
-            title='Setores Alvos'
-            onClick={() => setTitle('- Setores Alvos')}
-            className='link'
-            style={{ cursor: 'pointer' }}
-          >
-            <h2>Setores Alvo</h2>
-          </LinkScroll>
-          <LinkScroll
-            to='Contact'
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={1000}
-            title='Contatos'
-            onClick={() => setTitle('- Contatos')}
-            className='link'
-            style={{ cursor: 'pointer' }}
-          >
-            <h2>Contatos</h2>
-          </LinkScroll>
-          <Link to='/login' className='link'>
-            <h2>Login</h2>
-          </Link>
-        </div>
+
+        <Nav>
+          <MobileMenuToggle onClick={toggleMenu}>
+            {expanded ? <LuX /> : <LuMenu />}
+          </MobileMenuToggle>
+          <ContainerAdaptiveMenu $expanded={expanded}>
+            <LinkScroll
+              to='Functionality'
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={750}
+              title='Início'
+              onClick={() => setTitle('- Início')}
+              className='link'
+              style={{ cursor: 'pointer' }}
+            >
+              <h2>Início</h2>
+            </LinkScroll>
+            <LinkScroll
+              to='Functionality'
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={600}
+              title='Funcionalidades'
+              onClick={() => setTitle('- Funcionalidades')}
+              className='link'
+              style={{ cursor: 'pointer' }}
+            >
+              <h2>Funcionalidades</h2>
+            </LinkScroll>
+            <LinkScroll
+              to='TargetSectors'
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={1000}
+              title='Setores Alvos'
+              onClick={() => setTitle('- Setores Alvos')}
+              className='link'
+              style={{ cursor: 'pointer' }}
+            >
+              <h2>Setores Alvo</h2>
+            </LinkScroll>
+            <LinkScroll
+              to='Contact'
+              spy={true}
+              smooth={true}
+              offset={-50}
+              duration={1000}
+              title='Contatos'
+              onClick={() => setTitle('- Contatos')}
+              className='link'
+              style={{ cursor: 'pointer' }}
+            >
+              <h2>Contatos</h2>
+            </LinkScroll>
+            <Link to='/login' className='link'>
+              <h2>Login</h2>
+            </Link>
+          </ContainerAdaptiveMenu>
+        </Nav>
       </Navbar>
     </HeaderStyled>
   );
