ed9bd11d077d4d2054ffd1d7330a2bf8c9f1e5d1
Benfi4rthur <arthur_benfica@hotmail.com>
2023-08-03 20:51:06 -0300

    feat: criado backdrop atras do modal para que ao clicar nele não seja fechado o modal, porem apesar da logica estar correta, não esta funcional.

--- src/components/ModalPhoto/index.jsx
+++ src/components/ModalPhoto/index.jsx
@@ -16,6 +16,7 @@ import {
   ContainerProgressPercent,
   DialogContent,
   ImageAvatar,
+  Backdrop,
 } from './styled';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { SetNewValueDocument } from '../../utils/SetNewValueDocument';
@@ -34,8 +35,8 @@ export const DialogPhoto = ({
   const [open, setOpen] = useState(false);
   const [userImage, setUserImage] = useState([]);
   const [progressPercent, setProgressPercent] = useState(0);
-    const auth = getAuth();
-    const user = auth.currentUser;
+  const auth = getAuth();
+  const user = auth.currentUser;
 
   useEffect(() => {
     const func = async () => {
@@ -63,7 +64,6 @@ export const DialogPhoto = ({
   }
 
   async function handlePhoto() {
-
     const mediaThumb = document.getElementById('mediaThumb')?.files?.[0];
     const storage = getStorage();
     if (avatarName) {
@@ -93,65 +93,82 @@ export const DialogPhoto = ({
     }
   }
 
+  const handleBackdropClick = (event) => {
+    // Verifica se o clique foi diretamente no fundo e não dentro do modal
+    if (event.target === event.currentTarget) {
+      setOpen(true);
+      event.preventDefault(); // Impede o comportamento padrão do evento
+    }
+  };
+
   return (
+    <>
+      {open && <Backdrop onClick={handleBackdropClick} />}  
       <Dialog.Root open={open} onOpenChange={setOpen}>
         <Dialog.Trigger asChild>
           <ButtonActive {...rest}>{children}</ButtonActive>
         </Dialog.Trigger>
         <Dialog.Portal>
-          <DialogOverlay />
+        <DialogOverlay />
 
-          <DialogContent>
-            <Subtitle as={Dialog.Title} className='DialogTitle'>
-              Escolha seu Avatar:
-            </Subtitle>
+        <DialogContent>
+          <Subtitle as={Dialog.Title} className='DialogTitle'>
+            Escolha seu Avatar:
+          </Subtitle>
 
-            <ContainerButtonAvatar>
-              {Images.map((e, i) => (
-                <ButtonAvatar
-                  key={i}
-                  onClick={() => {
-                    setPhotoURL(e?.nameImage);
-                    setOpen(false);
-                  }}
-                >
-                  <ImageAvatar src={e?.url} alt='' />
-                </ButtonAvatar>
-              ))}
-              <>
-                {userImage.length > 0 && (
-                  <>
-                    {userImage.map((e, i) => (
-                      <ButtonAvatar key={i} onClick={handlePhoto}>
-                        <ImageAvatar src={e?.url} alt='' />
-                      </ButtonAvatar>
-                    ))}
-                  </>
-                )}
-              </>
-            </ContainerButtonAvatar>
+          <ContainerButtonAvatar>
+            {Images.map((e, i) => (
+              <ButtonAvatar
+                key={i}
+                onClick={() => {
+                  setPhotoURL(e?.nameImage);
+                  setOpen(false);
+                }}
+              >
+                <ImageAvatar src={e?.url} alt='' />
+              </ButtonAvatar>
+            ))}
+            <>
+              {userImage.length > 0 && (
+                <>
+                  {userImage.map((e, i) => (
+                    <ButtonAvatar key={i} onClick={handlePhoto}>
+                      <ImageAvatar src={e?.url} alt='' />
+                    </ButtonAvatar>
+                  ))}
+                </>
+              )}
+            </>
+          </ContainerButtonAvatar>
 
-            <CustomInputTypeFile
-              Svg={LuImagePlus}
-              placeholder='Adicionar Avatar'
-              title='Adicionar Avatar'
-              type='file'
-              id='mediaThumb'
-              accept='image/*'
-              onChange={handleGetUserImage}
-            />
+          <CustomInputTypeFile
+            Svg={LuImagePlus}
+            placeholder='Adicione um avatar e aguarde o carregamento...'
+            title='Adicionar Avatar'
+            type='file'
+            id='mediaThumb'
+            accept='image/*'
+            onChange={handleGetUserImage}
+          />
 
-            <ContainerProgressPercent>
-              {progressPercent >= 1 && <Progress value={progressPercent} min='0' max='100' />}
-            </ContainerProgressPercent>
+          <ContainerProgressPercent>
+            {progressPercent >= 1 && <Progress value={progressPercent} min='0' max='100' />}
+          </ContainerProgressPercent>
 
-            <Dialog.Close asChild onClick={()=>{setUserImage([])}}>
-              <IconButton aria-label='Close'>
-                <LuX />
-              </IconButton>
-            </Dialog.Close>
-          </DialogContent>
+          <Dialog.Close
+            asChild
+            onClick={() => {
+              setUserImage([]);
+            }}
+            title='Fechar'
+          >
+            <IconButton aria-label='Close'>
+              <LuX />
+            </IconButton>
+          </Dialog.Close>
+        </DialogContent>
         </Dialog.Portal>
       </Dialog.Root>
+    </>
   );
 };
--- src/components/ModalPhoto/styled.js
+++ src/components/ModalPhoto/styled.js
@@ -49,3 +49,13 @@ export const ContainerProgressPercent = styled.div`
   display: grid;
   place-items: center;
 `;
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+`;
