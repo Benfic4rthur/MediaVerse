eedfa8bade1f0873fcd545cb2babdcef3095d238
Benfi4rthur <arthur_benfica@hotmail.com>
2023-08-05 12:59:53 -0300

    fix: teste de alteraçãod e pasta.

--- src/pages/Access/index.jsx
+++ src/pages/Access/index.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { Contact } from '../../components/Access/Contact';
-import {Footer} from '../../components/Access/Footer';
+import { Footer } from '../../components/Access/Footer';
 
 import { useLoaderData } from 'react-router-dom';
 import { Functionality } from '../../components/Access/Functionality';
 import { Header } from '../../components/Access/Header';
-import {TargetSectors} from '../../components/Access/TargetSectors';
+import { TargetSectors } from '../../components/Access/TargetSectors';
 import { Video } from '../../components/Access/Video';
 import { Container } from './styled';
 
