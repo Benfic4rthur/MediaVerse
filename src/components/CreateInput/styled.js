cebf213d3224acfc9d12d6f23de76e08bf45cf4d
Benfi4rthur <arthur_benfica@hotmail.com>
2023-08-10 15:15:21 -0300

    feat: ajustada altura do container no mobile.

--- src/components/CreateInput/styled.js
+++ src/components/CreateInput/styled.js
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: relative;
   height: fit-content;
+  min-height: 1.5rem;
   z-index: 0;
   width: 100%;
   border-radius: ${({ theme }) => theme.border.radius};
+  @media screen and (max-width: 500px) {
+    height: fit-content;
+    min-height: 1.5rem;
+  }
 `;
-
