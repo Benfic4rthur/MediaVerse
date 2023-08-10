1b35a3868dded1f4a85a25f27d7ce0b7ec446647
Benfi4rthur <arthur_benfica@hotmail.com>
2023-08-10 15:22:11 -0300

    fix: altura setada sem fit content para o container do select.

--- src/components/CreateInput/styled.js
+++ src/components/CreateInput/styled.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
-  height: fit-content;
-  min-height: 1.5rem;
+  height: 4.1rem;
+  /* height: fit-content; */
   z-index: 0;
   width: 100%;
   border-radius: ${({ theme }) => theme.border.radius};
   @media screen and (max-width: 500px) {
-    height: fit-content;
-    min-height: 1.5rem;
+    height: 4.1rem;
+    min-height: 3.5rem;
   }
 `;
