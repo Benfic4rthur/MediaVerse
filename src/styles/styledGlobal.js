1e1b509f646d14a382f141337693f813e2a441bf
Benfi4rthur <arthur_benfica@hotmail.com>
2023-08-14 21:18:35 -0300

    fix: ajustada animação do spiner

--- src/styles/styledGlobal.js
+++ src/styles/styledGlobal.js
@@ -142,9 +142,6 @@ export const ContainerSpinerLoading = styled.div`
 
 export const SpinerLoading = styled(LuLoader2)`
   @keyframes spinAnimation {
-    0% {
-      transform: rotate(0deg);
-    }
     100% {
       transform: rotate(360deg);
     }
